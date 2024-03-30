c70ca031878c26f89cf456e01d2bbc401662ad2e
Bart van Wissen <bartvanwissen@gmail.com>
2024-03-30 17:53:55 +0000

    Play a sine wave

--- simple-synth/src/App.tsx
+++ simple-synth/src/App.tsx
@@ -1,29 +1,57 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function waveFunction(elapsedTime: number): number {
+  return Math.sin(2 * Math.PI * 440 * elapsedTime);
+}
+
 function App() {
   const [count, setCount] = useState(0)
-
   const [isPlaying, setIsPlaying] = useState(false);
 
+  const audioContextRef = useRef<AudioContext | null>(null);
+  const generatorRef = useRef<ScriptProcessorNode | null>(null);
+
   const handlePlayClick = () => {
-    setIsPlaying(!isPlaying);
+    audioContextRef.current = new AudioContext();
+
+    generatorRef.current = audioContextRef.current.createScriptProcessor(4096, 1, 1);
+    generatorRef.current.onaudioprocess = (audioProcessingEvent: AudioProcessingEvent): void => {
+      const outputBuffer = audioProcessingEvent.outputBuffer.getChannelData(0);
+      const sampleRate = audioContextRef.current?.sampleRate;
+
+      // Using Array.prototype.forEach for functional iteration
+      outputBuffer.forEach((_, index) => {
+        outputBuffer[index] = waveFunction(audioProcessingEvent.playbackTime + index / sampleRate!);
+      });
+    };
+
+    generatorRef.current.connect(audioContextRef.current.destination); // Connect the oscillator to the destination node
+
+    console.log("Playing");
+
+    setIsPlaying(true);
+    audioContextRef.current.resume();
+  };
+
+  const handleStopClick = () => {
+    generatorRef.current?.disconnect();
+    console.log("Stopped");
+    setIsPlaying(false);
   };
 
   return (
     <>
-      
       <h1>Simple Synth</h1>
       <div className="card">
-        <button onClick={handlePlayClick}>
+        <button onClick={isPlaying ? handleStopClick : handlePlayClick}>
           {isPlaying ? 'Stop' : 'Play'}
         </button>
       </div>
-      
     </>
   )
 }
 
-export default App
+export default App;
